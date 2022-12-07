4c5c15fc799d358ef4616306dcb7550175e0361a
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-12-07 09:40:51 -0800

    Stub plugin

---
+++ wp-test-56970.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Test for Trac 56970
+ * Description: Clears global stylesheet transient. See <a href="https://core.trac.wordpress.org/ticket/56970">Trac 56970</a>.
+ * Version: 0.1
+ */
+
