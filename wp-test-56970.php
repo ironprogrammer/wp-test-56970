a9b590d2789b5fc9cb7613419dd2819bb2b2d19e
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2022-12-09 16:27:57 -0800

    Rename function to avoid chance of core collision

--- wp-test-56970.php
+++ wp-test-56970.php
@@ -49,15 +49,9 @@ function wp_test_56970_transient_global_styles_stylesheet( $value, $transient ) 
 	return false;
 }
 
-add_action( '_core_updated_successfully', '_delete_global_styles_stylesheet_transient' );
-if ( ! function_exists( '_delete_global_styles_stylesheet_transient' ) ) {
-	/**
-	 * Deletes the global styles stylesheet transient.
-	 *
-	 * @since 6.1.2
-	 */
-	function _delete_global_styles_stylesheet_transient() {
-		$transient_name = 'global_styles_' . get_stylesheet();
-		delete_site_transient( "transient_{$transient_name}" );
-	}
+// Hook to delete the global styles stylesheet transient after core update.
+add_action( '_core_updated_successfully', 'wp_test_56970_delete_global_styles_stylesheet_transient' );
+function wp_test_56970_delete_global_styles_stylesheet_transient() {
+	$transient_name = 'global_styles_' . get_stylesheet();
+	delete_site_transient( "transient_{$transient_name}" );
 }
